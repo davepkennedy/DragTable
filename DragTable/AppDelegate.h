e707a80627b2d2935933283b3155cd16037e1bfe
Dave Kennedy <davepkennedy@gmail.com>
2015-05-22 20:44:46 +0100

    Initial Commit

---
+++ DragTable/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DragTable
+//
+//  Created by Dave Kennedy on 22/05/2015.
+//  Copyright (c) 2015 Dave Kennedy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ DragTable/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  DragTable
+//
+//  Created by Dave Kennedy on 22/05/2015.
+//  Copyright (c) 2015 Dave Kennedy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
